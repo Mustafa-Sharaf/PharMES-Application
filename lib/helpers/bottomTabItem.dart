39261aeeab1500933024c1f996f496055afc821d
ASUS <mustafasharaf100@gmail.com>
2025-06-11 22:02:47 +0300

    add qrCode

---
+++ lib/helpers/bottomTabItem.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../app_theme/app_colors.dart';
+import '../modules/home_page/home_controller.dart';
+
+
+class BottomTabItem extends StatelessWidget {
+  final int index;
+  final IconData icon;
+  final String label;
+  final BottomNavController controller = Get.find();
+
+  BottomTabItem({
+    super.key,
+    required this.index,
+    required this.icon,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      final isSelected = controller.currentIndex.value == index;
+      return GestureDetector(
+        onTap: () => controller.changeTabIndex(index),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, color: isSelected ? AppColors.primaryColor : Colors.grey,size:30,),
+            Text(label,
+              style: TextStyle(
+                fontSize: 12,
+                color: isSelected ? AppColors.primaryColor : Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
