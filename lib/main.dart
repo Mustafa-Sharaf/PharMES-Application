602839f3e4756974f36e605ea559c19e2562ced1
ASUS <mustafasharaf100@gmail.com>
2025-06-13 12:25:44 +0300

    create update lang by mustafa

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/home',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: AppColors.primaryColor),
-      //locale:Get.deviceLocale,
+      locale:Get.deviceLocale,
       translations: MyLanguage(),
       getPages: [
         GetPage(name: '/signIn', page: ()=>SignInScreen(),binding: SignInBindings()),
